6453d6d54b71bb585fbb52fb8393d4b3b9a967c1
orectique <49713741+orectique@users.noreply.github.com>
2021-05-27 18:56:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+import pandas as pd
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+df = pd.read_csv('https://raw.githubusercontent.com/orectique/protestindex/main/Factors.csv')
+
+available_countries = df['Country'].unique()
+
+app.layout = html.Div([
+    
+    dcc.Dropdown(
+                id='country',
+                options=[{'label': i, 'value': i} for i in available_countries],
+                value= ['Afghanistan', 'Japan'],
+                multi = True
+            ),
+
+
+
+    dcc.Graph(id='indicator-graphic'),
+
+    dcc.RangeSlider(
+        id='year--slider',
+        min=df['Year'].min(),
+        max=df['Year'].max(),
+        value=[df['Year'].min(), df['Year'].max()],
+        marks={str(year): str(year) for year in df['Year'].unique()},
+        step=None
+    )
+])
+
+@app.callback(
+    Output('indicator-graphic', 'figure'),
+    Input('country', 'value'),
+    Input('year--slider', 'value'))
+def update_graph(country_names, year_value):
+    year_list = list( i for i in range(year_value[0], year_value[1] + 1))
+    df1 = df[df['Country'].isin(list(country_names))]
+    dff = df1[df1['Year'].isin(year_list)]
+    fig = px.scatter(dff, x="Factor1", y="Factor2", color = 'Country', hover_data=['Year'])
+    
+
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
