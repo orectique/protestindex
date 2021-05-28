17d4097e6359a855a6b8787a5e0f727e8ca3e176
orectique <49713741+orectique@users.noreply.github.com>
2021-05-28 20:03:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import random
 
 external_stylesheets = ['https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/cyborg/bootstrap.min.css']
 app = dash.Dash(__name__, meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}], external_stylesheets=external_stylesheets)
-app.title = 'Protest Index'
+app.title = 'Protest Index Try'
 server = app.server
 
 df = pd.read_csv('https://raw.githubusercontent.com/orectique/protestindex/main/Factors.csv')
@@ -17,50 +17,62 @@ df = pd.read_csv('https://raw.githubusercontent.com/orectique/protestindex/main/
 available_countries = df['Country'].unique()
 rand_country = random.choice(available_countries)
 
+
+########################################################
+
 app.layout = html.Div([
-    
-    html.Div(
-            [  dcc.Markdown(
+
+    html.Div([
+        #Title, subtitle, button
+        dcc.Markdown(
                             """
-                ### Quantifying events of Social Unrest:
-                
-                ### Protest Index
+                ### Quantifying events of Social Unrest: Protest Index
                 """.replace(
                                 "  ", ""
                             ),
-                            className="title",
+                            style = {'font-family' : '"Times New Roman", Times, serif', 'padding': '20px 10px 20px 30px'},
                         ),
-                        dcc.Markdown(
-                            """This interactive report is a rendition of a study by Sreehari P Sreedhar.""".replace(
+        dcc.Markdown(
+                            """This interactive graph is a rendition of a study by Sreehari P Sreedhar in which they explored the creation
+                            of a new indexing system to effectively capture the scale and hence enable the comparison of social unrest across countries
+                            and years. """.replace(
                                 "  ", ""
                             ),
-                            className="subtitle",
+                            style = {'font-family' : '"Times New Roman", Times, serif', 'padding': '20px 10px 20px 20px'},
                         ),
 
-                html.Div([
-                        html.A(
+        html.Div([
+            html.A(
                             html.Button("View the Code Notebook on GitHub.", className="learn-more-button"),
                             href="https://github.com/orectique/protestindex/blob/main/Code%20Notebook.txt",
                             target="_blank",
                         )
                     ],
-                    className="info-button",
+                    style = {'font-family' : '"Times New Roman", Times, serif', 'display':'inline-block', 'padding':'30px 10px 30px 20px' },
                 ),
 
-
-
-    
-      html.Div([dcc.Dropdown(
+        html.Div([
+        #country select
+        dcc.Dropdown(
                 id='country',
                 options=[{'label': i, 'value': i} for i in available_countries],
                 value= [rand_country],
                 multi = True,
                 clearable=False
             ),
+    ], style = {'width' : '100%', 'float':'left', 'display': 'inline-block', 'padding': '30px 10px 30px 20px'}),
 
-    ],style={'width': '7%', 'float': 'left', 'display': 'inline-block', 'padding': '25px 30px 25px 35px'}),
+    ], style = {'width' : '30%', 'float': 'left', 'display': 'inline-block'}),
 
-        html.Div([dcc.RangeSlider(
+
+    html.Div([
+        #graph
+        dcc.Graph(id='indicator-graphic')
+    ], style = {'width' : '70%', 'height':'100%', 'float': 'right', 'display': 'inline-block', 'padding': '150px 10px 25px 10px'}),
+
+    html.Div([
+        #timeline
+        dcc.RangeSlider(
         id='year--slider',
         min=df['Year'].min(),
         max=df['Year'].max(),
@@ -68,19 +80,11 @@ app.layout = html.Div([
         marks={str(year): str(year) for year in df['Year'].unique()},
         step=None
     )
-    ], style={'width': '100%', 'float': 'bottom', 'display': 'inline-block', 'padding': '25px 10px 25px 10px'})
-            ], style={'width': '50%', 'float': 'left', 'display': 'inline-block', 'padding': '25px 30px 25px 10px'}),
-
-    html.Div([
-    html.Div([
-        dcc.Graph(id='indicator-graphic')
-        ],style={'width': '100%', 'float': 'centre', 'display': 'inline-block'}),
-    ], style={'width': '50%', 'float': 'right', 'display': 'inline-block', 'padding': '25px 10px 25px 30px'}),
-
-
+    ], style = {'width' : '100%', 'margin':'auto', 'display': 'inline-block', 'padding': '25px 40px 25px 40px'})
+  
 ])
 
-#style={'width': '45%', 'float': 'right', 'display': 'inline-block'})
+########################################################
 
 @app.callback(
     Output('indicator-graphic', 'figure'),
