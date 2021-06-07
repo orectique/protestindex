23e220d53aad9a31d1d5a34a698cffd7c06a83ff
orectique <49713741+orectique@users.noreply.github.com>
2021-06-07 14:11:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import pandas as pd
 import random
 
 app = dash.Dash(__name__, meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}], external_stylesheets=[dbc.themes.SUPERHERO])
-app.title = 'Unrest Index'
+app.title = 'Unrest Evaluation Index'
 server = app.server
 
 df = pd.read_csv('https://raw.githubusercontent.com/orectique/protestindex/main/Factors.csv')
